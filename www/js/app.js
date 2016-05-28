dc4c6232591a8fefc5d48859387c4be57b5c3433
bechir <bechir@accessnow.org>
2016-05-28 20:30:25 +0100

    CVE Life cycle

--- www/js/app.js
+++ www/js/app.js
@@ -4,8 +4,26 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
-
+angular.module('starter', ['ionic', 'ngStorage','starter.controllers'])
+.factory ('StorageService', function ($localStorage) {
+  $localStorage = $localStorage.$default({
+    products: []
+  });
+  var _getAll = function () {
+    return $localStorage.products;
+  };
+  var _add = function (product) {
+    $localStorage.products.push(product);
+  }
+  var _remove = function (product) {
+    $localStorage.products.splice($localStorage.products.indexOf(product), 1);
+  }
+  return {
+      getAll: _getAll,
+      add: _add,
+      remove: _remove
+    };
+})
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -40,34 +58,53 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     }
   })
-
-  .state('app.browse', {
-      url: '/browse',
+  
+    .state('app.subscription', {
+      url: '/subscriptions',
       views: {
         'menuContent': {
-          templateUrl: 'templates/browse.html'
+          templateUrl: 'templates/subscriptions.html',
+          controller: 'SubscriptionCtrl'
         }
       }
     })
-    .state('app.playlists', {
-      url: '/playlists',
+    .state('app.browse', {
+      url: '/browse',
       views: {
         'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
+          templateUrl: 'templates/browse.html',
+          controller: 'VendorsCtrl'
+        }
+      }
+    })
+    .state('app.products', {
+      url: '/browse/:name',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/products.html',
+          controller: 'ProductsCtrl'
+        }
+      }
+    })
+    .state('app.cves', {
+      url: '/cves',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/cves.html',
+          controller: 'CvesCtrl'
         }
       }
     })
 
   .state('app.single', {
-    url: '/playlists/:playlistId',
+    url: '/cves/:id',
     views: {
       'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
+        templateUrl: 'templates/cve.html',
+        controller: 'CveCtrl'
       }
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/cves');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,124 @@
 angular.module('starter.controllers', [])
+        
+        .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+          // With the new view caching in Ionic, Controllers are only called
+          // when they are recreated or on app start, instead of every page change.
+          // To listen for when this page is active (for example, to refresh data),
+          // listen for the $ionicView.enter event:
+          //$scope.$on('$ionicView.enter', function(e) {
+          //});
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+          // Form data for the login modal
+          $scope.loginData = {};
 
-  // Form data for the login modal
-  $scope.loginData = {};
+          // Create the login modal that we will use later
+          $ionicModal.fromTemplateUrl('templates/login.html', {
+            scope: $scope
+          }).then(function (modal) {
+            $scope.modal = modal;
+          });
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+          // Triggered in the login modal to close it
+          $scope.closeLogin = function () {
+            $scope.modal.hide();
+          };
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+          // Open the login modal
+          $scope.login = function () {
+            $scope.modal.show();
+          };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+          // Perform the login action when the user submits the login form
+          $scope.doLogin = function () {
+            console.log('Doing login', $scope.loginData);
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+            // Simulate a login delay. Remove this and replace with your login
+            // code if using a login system
+            $timeout(function () {
+              $scope.closeLogin();
+            }, 1000);
+          };
+        })
+        
+        .controller('VendorsCtrl', ['$scope', '$http', function ($scope, $http) {
+          
+          fetchVendors = function () {
+            $scope.code = null;
+            $scope.response = null;
+            $http({method: 'GET', url: 'https://cve.circl.lu/api/browse'}).
+                    then(function (response) {
+                      $scope.status = response.status;
+                      $scope.vendor = response.data;
+                    }, function (response) {
+                      $scope.vendor = response.data || "Request failed";
+                      $scope.status = response.status;
+                    });
+          };
+          fetchVendors();
+        }])
+        
+        .controller('ProductsCtrl', function ($scope, $stateParams, $http, StorageService) {
+          $scope.saveProduct = function(product){
+            StorageService.add({vendor: $stateParams.name, product: product});
+          };
+          fetchProducts = function (name) {
+            $scope.code = null;
+            $scope.response = null;
+            $http({method: 'GET', url: 'https://cve.circl.lu/api/browse/' + name}).
+                    then(function (response) {
+                      $scope.status = response.status;
+                      $scope.products = response.data;
+                    }, function (response) {
+                      $scope.cves = response.data || "Request failed";
+                      $scope.products = response.status;
+                    });
+          };
+          fetchProducts($stateParams.name);
+        })
+      
+        .controller('SubscriptionCtrl', ['$scope', 'StorageService', function ($scope, StorageService) {
+          $scope.removeProduct = function(product){
+            StorageService.remove(product);
+            console.log(StorageService.getAll());
+          };
+          $scope.products = StorageService.getAll();
+        }])
+      
+        .controller('CvesCtrl', ['$scope', '$http', 'StorageService', function ($scope, $http, StorageService) {
+          var config = StorageService.getAll();
+          $scope.cves = [];
+          fetch = function (vendor, product, $scope) {
+            $scope.code = null;
+            $scope.response = null;
+            $http({method: 'GET', url: 'https://cve.circl.lu/api/search/' + vendor + '/' + product}).
+                    then(function (response) {
+                      $scope.status = response.status;
+                      angular.forEach(response.data, function(value, key) {
+                        value.product = product;
+                        value.vendor = vendor;
+                        $scope.cves.push(value);
+                      });      
+                    });
+          };
+          angular.forEach(config, function(value, key) {
+            fetch(value.vendor, value.product, $scope);
+          });          
+        }])
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
-
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+        .controller('CveCtrl', function ($scope, $stateParams, $http) {
+          console.log($stateParams);
+          fetchOne = function (id) {
+            $scope.code = null;
+            $scope.response = null;
+            $http({method: 'GET', url: 'https://cve.circl.lu/api/cve/' + id}).
+                    then(function (response) {
+                      $scope.status = response.status;
+                      $scope.cve = response.data;
+                    }, function (response) {
+                      $scope.cve = response.data || "Request failed";
+                      $scope.status = response.status;
+                    });
+          };
+          fetchOne($stateParams.id);
+        });
